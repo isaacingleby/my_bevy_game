316926e057125881b3fd2bf386f15bbecaa7713d
isaacingleby <isaacingleby@gmail.com>
2024-04-01 17:29:03 +0100

    Plugins tutorial recommended changes

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,9 @@ fn update_people(mut query: Query<&mut Name, With<Person>>) {
     for mut name in &mut query {
         if name.0 == "Elaina Proctor" {
             name.0 = "Elaina Hume".to_string();
-            break // only update one person
+            break; // only update one person
         }
     }
-
 }
 
 fn greet_people(query: Query<&Name, With<Person>>) {
@@ -28,16 +27,21 @@ fn greet_people(query: Query<&Name, With<Person>>) {
     }
 }
 
-
 fn hello_world() {
     println!("Hello, world!");
 }
 
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, add_people)
+            // chain allows us to specify the order of the systems running, otherwise they run in
+            // parallel, with no guaranteed order
+            .add_systems(Update, (hello_world, (update_people, greet_people).chain()));
+    }
+}
+
 fn main() {
-    App::new()
-        .add_systems(Startup, add_people)
-        // chain allows us to specify the order of the systems running, otherwise they run in
-        // parallel, with no guaranteed order
-        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
-        .run();
+    App::new().add_plugins((DefaultPlugins, HelloPlugin)).run();
 }
